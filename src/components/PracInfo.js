5fc4fc642848db11c2ed33ead89b415b3d201fe9
Dark-WizZ <Dark-WizZ>
2023-01-18 08:00:30 +0530

    Convert PracInfo class component to functional

--- src/components/PracInfo.js
+++ src/components/PracInfo.js
@@ -1,73 +1,70 @@
-import React from "react";
+import React, {useState} from "react";
 
-export default class PracInfo extends React.Component{
+function PracInfo(){
+  const [state, setState] = useState({
+    companyName: '',
+    positionTitle: '',
+    mainTask: '',
+    startDate: '',
+    endDate: '',
+    edit: true,
+  });
 
-  constructor(props){
-    super(props);
-    this.state = {
-      companyName: '',
-      positionTitle: '',
-      mainTask: '',
-      startDate: '',
-      endDate: '',
-      edit: true,
-    }
+  let companyNameIp, positionTitleIp, mainTaskIp, startDateIp, endDateIp;
+
+  const domCache = () => {
+    companyNameIp = document.querySelector('#company_name_ip');
+    positionTitleIp = document.querySelector('#position_title_ip');
+    mainTaskIp = document.querySelector('#main_task_ip');
+    startDateIp = document.querySelector('#start_date_ip');
+    endDateIp = document.querySelector('#end_date_ip');
   }
 
-  domCache(){
-    this.companyNameIp = document.querySelector('#company_name_ip');
-    this.positionTitleIp = document.querySelector('#position_title_ip');
-    this.mainTaskIp = document.querySelector('#main_task_ip');
-    this.startDateIp = document.querySelector('#start_date_ip');
-    this.endDateIp = document.querySelector('#end_date_ip');
-  }
-
-  submitClk = (e) => {
+  const submitClk = (e) => {
     e.preventDefault();
-    this.domCache();
-    const {edit} = this.state;
+    domCache();
+    const {edit} = state;
     if(edit){
-      this.setState({
-        companyName: this.companyNameIp.value,
-        positionTitle: this.positionTitleIp.value,
-        mainTask: this.mainTaskIp.value,
-        startDate: this.startDateIp.value,
-        endDate: this.endDateIp.value,
+      setState({
+        companyName: companyNameIp.value,
+        positionTitle: positionTitleIp.value,
+        mainTask: mainTaskIp.value,
+        startDate: startDateIp.value,
+        endDate: endDateIp.value,
         edit:false,
       })
     }else{
-      this.setState({edit:true})
+      setState({
+        ...state,
+        edit:true
+      })
     }
   }
 
-  render(){
-    return (this.state.edit)? this.input(): this.output();
-  }
-
-  input(){
+  const input = () => {
     return(
       <div className="practical">
         <h1 className="heading">PRACTICAL INFORMATION:</h1>
-        <form className="info ip" onSubmit={this.submitClk}>
+        <form className="info ip" onSubmit={submitClk}>
           <label htmlFor='company_name_ip'>Comapany Name: </label>
           <input type='text' id='company_name_ip' 
-            defaultValue={this.state.companyName}
+            defaultValue={state.companyName}
           ></input>
           <label htmlFor='position_title_ip'>Position Title: </label>
           <input type='text' id='position_title_ip'
-            defaultValue={this.state.positionTitle}
+            defaultValue={state.positionTitle}
           ></input>
           <label htmlFor='main_task_ip'>Main Task: </label>
           <input type='text' id='main_task_ip'
-            defaultValue={this.state.mainTask}
+            defaultValue={state.mainTask}
           ></input>
           <label htmlFor='start_date_ip'>Start Date: </label>
           <input type='date' id='start_date_ip'
-            defaultValue={this.state.startDate}
+            defaultValue={state.startDate}
           ></input>
           <label htmlFor='end_date_ip'>End Date: </label>
           <input type='date' id='end_date_ip'
-            defaultValue={this.state.endDate}
+            defaultValue={state.endDate}
           ></input>
           <button className="submit btn">SUBMIT</button>
         </form>
@@ -75,24 +72,29 @@ export default class PracInfo extends React.Component{
     )
   }
 
-  output(){
+  const output = () => {
     return(
       <div className="practical">
         <h1 className="heading">PRACTICAL INFORMATION:</h1>
-        <form className="info op" onSubmit={this.submitClk}>
+        <form className="info op" onSubmit={submitClk}>
           <label htmlFor='company_name_op'>Comapany Name: </label>
-          <div id='company_name_op'>{this.state.companyName}</div>
+          <div id='company_name_op'>{state.companyName}</div>
           <label htmlFor='position_title_op'>Position Title: </label>
-          <div id='position_title_op'>{this.state.positionTitle}</div>
+          <div id='position_title_op'>{state.positionTitle}</div>
           <label htmlFor='main_task_op'>Main Task: </label>
-          <div id='main_task_op'>{this.state.mainTask}</div>
+          <div id='main_task_op'>{state.mainTask}</div>
           <label htmlFor='start_date_op'>Start Date: </label>
-          <div id='start_date_op'>{this.state.startDate}</div>
+          <div id='start_date_op'>{state.startDate}</div>
           <label htmlFor='end_date_op'>End Date: </label>
-          <div id='end_date_op'> {this.state.endDate}</div>
+          <div id='end_date_op'> {state.endDate}</div>
           <button className="edit btn">EDIT</button>
         </form>
       </div>
     )
   }
+
+  return (state.edit)? input(): output();
+
 }
+
+export default PracInfo;
