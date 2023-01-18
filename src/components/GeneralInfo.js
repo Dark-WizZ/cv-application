9bff07354e5c1be98574c027751c7828e2aef569
Dark-WizZ <Dark-WizZ>
2023-01-18 07:52:18 +0530

    Convert GenrealInfo class to functional component

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -1,73 +1,74 @@
-import React from "react";
+import React,{useState} from "react";
 
-export default class GeneralInfo extends React.Component{
+function GeneralInfo(){
+  const [state, setState] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    edit: true,
+  })
 
-  constructor(props){
-    super(props);
-    this.state = {
-      name: '',
-      email: '',
-      phone: '',
-      edit: true,
-    }
+  let nameIp; let emailIp; let phoneIp;
+
+  const domCache = () => {
+    nameIp = document.querySelector('#name_ip');
+    emailIp = document.querySelector('#email_ip');
+    phoneIp = document.querySelector('#phone_ip');
   }
 
-  domCache(){
-    this.nameIp = document.querySelector('#name_ip');
-    this.emailIp = document.querySelector('#email_ip');
-    this.phoneIp = document.querySelector('#phone_ip');
-  }
-
-  submitClk = (e) => {
+  const submitClk = (e) => {
     e.preventDefault();
-    this.domCache();
-    const {edit} = this.state;
+    domCache();
+    const {edit} = state;
     if(edit){
-      this.setState({
-        name:this.nameIp.value,
-        email:this.emailIp.value,
-        phone:this.phoneIp.value,
+      setState({
+        name:nameIp.value,
+        email:emailIp.value,
+        phone:phoneIp.value,
         edit:false,
       })
     }else{
-      this.setState({edit:true})
+      setState({
+        ...state,
+        edit:true
+      })
     }
   }
 
-  render(){
-    return (this.state.edit)? this.input(): this.output();
-  }
-
-  input(){
+  const input = () => {
     return(
       <div className="general">
         <h1 className="heading">GENERAL INFORMATION:</h1>
-        <form className="info ip" onSubmit={this.submitClk}>
+        <form className="info ip" onSubmit={submitClk}>
           <label htmlFor='name_ip'>Name: </label>
-          <input type='text' id='name_ip' defaultValue={this.state.name}></input>
+          <input type='text' id='name_ip' defaultValue={state.name}></input>
           <label htmlFor='email_ip'>Email: </label>
-          <input type='text' id='email_ip' defaultValue={this.state.email}></input>
+          <input type='text' id='email_ip' defaultValue={state.email}></input>
           <label htmlFor='phone_ip'>Phone: </label>
-          <input type='number' id='phone_ip' defaultValue={this.state.phone}></input>
+          <input type='number' id='phone_ip' defaultValue={state.phone}></input>
           <button className="submit btn">SUBMIT</button>
         </form>
       </div>
     )
   }
-  output(){
+  const output= () => {
     return(
       <div className="general">
         <h1 className="heading">GENERAL INFORMATION:</h1>
-        <form className="info op" onSubmit={this.submitClk}>
+        <form className="info op" onSubmit={submitClk}>
           <label htmlFor='name_op'>Name: </label>
-          <div className='name_op'>{this.state.name}</div>
+          <div className='name_op'>{state.name}</div>
           <label htmlFor='email_op'>Email: </label>
-          <div className='email_op'>{this.state.email}</div>
+          <div className='email_op'>{state.email}</div>
           <label htmlFor='phone_op'>Phone: </label>
-          <div className='phone_op'>{this.state.phone}</div>
+          <div className='phone_op'>{state.phone}</div>
           <button className="edit btn">EDIT</button>
         </form>
       </div>
     )
   }
+
+  return (state.edit)? input(): output();
 }
+
+export default GeneralInfo;
