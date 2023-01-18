ae1867c1d8a4ccadfa49e0682e1ff061dc94dfea
Dark-WizZ <Dark-WizZ>
2023-01-18 08:07:20 +0530

    Convert App class component to functional component

--- src/App.js
+++ src/App.js
@@ -4,22 +4,14 @@ import GeneralInfo from './components/GeneralInfo'
 import EduInfo from './components/EduInfo';
 import PracInfo from './components/PracInfo';
 
-class App extends React.Component{
-
-  submitClk = (e) => {
-    e.preventDefault();
-    this.state.edit? this.setState({edit:false}): this.setState({edit:true})
-  }
-
-  render(){
-    return(
-      <div className='app'>
-        <GeneralInfo submitClk={this.submitClk}/>
-        <EduInfo />
-        <PracInfo />
-      </div>
-    )
-  }
+function App(){
+  return(
+    <div className='app'>
+      <GeneralInfo/>
+      <EduInfo />
+      <PracInfo />
+    </div>
+  )
 }
 
 export default App;
